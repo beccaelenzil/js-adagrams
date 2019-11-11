67973aa351c94c435e3d1af1aa13f31073e06820
beccaelenzil <beccaelenzil@gmail.com>
2019-11-11 11:04:31 -0800

    waves 1 and 2

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,8 +1,55 @@
+import { SSL_OP_LEGACY_SERVER_CONNECT } from "constants";
+
+const letterCount= {
+  A: 9, B: 2, C: 2, D: 4, E: 12, F: 2, G: 3, H: 2, I: 9, J: 1, K: 1, L: 4, M: 2, N: 6, O: 8, P: 2, Q: 1, R: 6, S: 4, T: 6, U: 4, V: 2, W: 2, X: 1, Y: 2, Z: 1
+};
+
+const letterValue = {
+  A: 1, B: 3, C: 3, D: 2, E: 1, F: 4, G: 2, H: 4, I: 1, J: 8, K: 5, L: 1, M: 3, N: 1, O: 1, P: 3, Q: 10, R: 1, S: 1, T: 1, U: 1, V: 4, W: 4, X: 8, Y: 4, Z: 10
+};
+
+const alphabet = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase().split('')
+
+let letterPool = []
+alphabet.forEach( function(letter){
+  for (let i = 0; i < letterCount[letter]; i++){
+    letterPool.push(letter)
+  }
+});
+
+
+
 const Adagrams = {
   drawLetters() {
-    // Implement this method for wave 1
+    let hand = []
+    let letterPoolCopy = [...letterPool]
+    for (let i = 0; i < 10; i++){
+      let n = letterPoolCopy.length
+      let i = Math.floor(n*Math.random()) 
+      hand.push(letterPoolCopy[i])
+      letterPoolCopy.splice(i,1)
+    };
+    return hand
   },
-};
+
+  usesAvailableLetters(input, lettersInHand){
+    const input_array = input.split('')
+    const n = input.length
+    let lettersInHandCopy = [...lettersInHand]
+    for (let i = 0; i < n; i++){
+      let letter = input_array[i]
+      if (lettersInHandCopy.includes(letter)){
+        let j = lettersInHandCopy.indexOf(letter)
+        lettersInHandCopy.splice(j, 1)
+      }
+      else{
+        return false
+      }
+    };
+  return true
+  }
+}
+
 
 // Do not remove this line or your tests will break!
 export default Adagrams;
--- test/adagrams.test.js
+++ test/adagrams.test.js
@@ -45,7 +45,7 @@ describe('Adagrams', () => {
     });
   });
 
-  describe('scoreWord', () => {
+  describe.skip('scoreWord', () => {
     const expectScores = (wordScores) => {
       Object.entries(wordScores).forEach(([word, score]) => {
         expect(Adagrams.scoreWord(word)).toBe(score);
